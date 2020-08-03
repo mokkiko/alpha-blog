3a153cf581777e3a3c331cfc4e44c4595e3142d9
mokkiko <maksim_pro@mail.ru>
2020-08-03 07:56:06 +0200

    add edit   -- CRUD

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,17 +15,33 @@ def new
 end
 
 
+def edit
+#byebug
+  @article = Article.find(params[:id])
+end
+
+
 def create
   @article = Article.new(params.require(:article).permit(:title, :discription))
   if @article.save
-      flash[:notice] = "article was created secsesfully."
+      flash[:notice] = "article was created seccesfully."
       redirect_to @article
   else
       render 'new'
-
   end
 end
 
+def update
+#  byebug
+  @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :discription))
+      flash[:notice] = "articl updated successfully"
+      redirect_to @article
+    else
+        render 'edit'
+    end
+
+  end
 
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root 'pages#home'
 	get 'about', to: 'pages#about'
   get 'mtest', to: 'pages#mtest'
-  resources :articles, only: [:show, :index, :new, :create]
+  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
   resources :maxtest, only: [:show, :inde, :mtest]
 
 end
