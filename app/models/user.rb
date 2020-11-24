0a1e758b1d251c960c4a56db0ee6567c39ecda2c
mokkiko <maksim_pro@mail.ru>
2020-11-24 09:00:53 +0300

    fixed dowbcase metod and show user info in views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 
 class User < ApplicationRecord
-  has_many :articles 
+
+  before_save { self.email = email.downcase }
+  has_many :articles
   validates :username, presence: true,
                       uniqueness: { case_sensitive: false },
                       length: { minimum: 3, maximum: 25 }
