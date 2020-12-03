7e882bde8d1b93a6a740c20863d42ece1e7fe873
mokkiko <maksim_pro@mail.ru>
2020-12-03 11:41:49 +0300

    03.12

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,8 @@ Rails.application.routes.draw do
   #resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
   resources :articles
   resources :maxtest, only: [:show, :inde, :mtest]
-
+  get 'signup', to: 'users#new'
+  #post 'users', to: 'users#create'
+  # to je samoe chto stroka vnizu
+  resources :users, except: [:new]
 end
